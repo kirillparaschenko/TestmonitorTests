acb2e899fa93ead7aaf07c2d4d2efffcdc461523
Kirill Paraschenko <kparaschenko@caeonline.com>
2023-06-30 19:47:56 +0300

    Added BasePage, UIElement

---
+++ TestmonitorTests/Pages/BasePage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestmonitorTests.Core;
+using TestmonitorTests.Utilities.Configuration;
+
+namespace TestmonitorTests.Pages
+{
+    public abstract class BasePage
+    {
+        protected IWebDriver Driver;
+        protected WaitService WaitService;
+
+        public BasePage(IWebDriver driver, bool openPageByUrl)
+        {
+            Driver = driver;
+            WaitService = new WaitService(Driver);
+
+            if (openPageByUrl)
+            {
+                OpenPageByUrl();
+            }
+        }
+
+        public abstract bool IsPageOpened();
+        protected abstract string GetEndpoint();
+
+        private void OpenPageByUrl()
+        {
+            Driver.Navigate().GoToUrl(Configurator.AppSettings.URL + GetEndpoint());
+        }
+    }
+}
---
+++ TestmonitorTests/Pages/LoginPage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestmonitorTests.Pages
+{
+    public class LoginPage : BasePage
+    {
+        public LoginPage(IWebDriver driver, bool openPageByUrl) : base(driver, openPageByUrl)
+        {
+        }
+
+        public override bool IsPageOpened()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetEndpoint()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
