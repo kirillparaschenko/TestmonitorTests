2de3873ccce736a09dfd82def08a1cd7c29bf044
kirillparaschenko <kparaschenko@caeonline.com>
2023-07-02 12:51:05 +0300

    Added Locators and Methods for Login Page

--- TestmonitorTests/Pages/LoginPage.cs
+++ TestmonitorTests/Pages/LoginPage.cs
@@ -4,23 +4,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestmonitorTests.Wrappers;
 
 namespace TestmonitorTests.Pages
 {
     public class LoginPage : BasePage
     {
+        private static string END_POINT = "";
+
+        // Locators
+        private static readonly By EmailInputBy = By.Id("email");
+        private static readonly By PswInputBy = By.Id("password");
+        private static readonly By RememberMeCheckboxBy = By.Name("remember");
+        private static readonly By LoginInButtonBy = By.ClassName("button is-primary is-fullwidth");
+
         public LoginPage(IWebDriver driver, bool openPageByUrl) : base(driver, openPageByUrl)
+        {
+        }
+
+        public LoginPage(IWebDriver driver) : base(driver, false)
         {
         }
 
         public override bool IsPageOpened()
         {
-            throw new NotImplementedException();
+            return WaitService.GetVisibleElement(LoginInButtonBy) != null;
         }
 
         protected override string GetEndpoint()
         {
-            throw new NotImplementedException();
+            return END_POINT;
+        }
+
+        //Methods
+        public Input EmailInput()
+        {
+            return new Input(Driver, EmailInputBy);
+        }
+
+        public Input PswInput()
+        {
+            return new Input(Driver, PswInputBy);
+        }
+
+        public UIElement RememberMeCheckbox()
+        {
+            return new UIElement(Driver, RememberMeCheckboxBy);
+        }
+
+        public Button LoginInButton()
+        {
+            return new Button(Driver, LoginInButtonBy);
         }
     }
 }
