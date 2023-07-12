920bf5d1eb999f9922e9e153d5f9932b9d22062f
Kirill Paraschenko <kparaschenko@caeonline.com>
2023-07-12 19:49:14 +0300

    Added allure attribute to BaseApiTest

--- TestmonitorTests/Tests/API/BaseApiTest.cs
+++ TestmonitorTests/Tests/API/BaseApiTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Allure.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ using TestmonitorTests.Services;
 
 namespace TestmonitorTests.Tetsts.API
 {
+    [AllureNUnit]
     [Parallelizable(ParallelScope.Fixtures)]
     public class BaseApiTest
     {
